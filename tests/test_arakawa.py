fb015873a6b3a5df442299c9fc138a3c9c147363
Constantin Gahr <constantin.gahr@ipp.mpg.de>
2023-08-03 11:53:19 +0200

    fixed a bug in the test

--- tests/test_arakawa.py
+++ tests/test_arakawa.py
@@ -1,9 +1,10 @@
 from unittest import TestCase
+
 import numpy as np
-from hw2d.initializations.sine import get_2d_sine
+from hw2d.arakawa.numba_arakawa import periodic_arakawa_stencil as periodic_arakawa_nb
 from hw2d.arakawa.numpy_arakawa import periodic_arakawa as periodic_arakawa
 from hw2d.arakawa.numpy_arakawa import periodic_arakawa_vec as periodic_arakawa_np
-from hw2d.arakawa.numba_arakawa import periodic_arakawa_stencil as periodic_arakawa_nb
+from hw2d.initializations.sine import get_2d_sine
 
 
 class test_arakawa(TestCase):
@@ -18,4 +19,5 @@ class test_arakawa(TestCase):
         arr1 = periodic_arakawa(zeta, psi, dx)
         arr2 = periodic_arakawa_np(zeta, psi, dx)
         arr3 = periodic_arakawa_nb(zeta, psi, dx)
-        assert np.allclose(arr1, arr2, arr3)
+        assert np.allclose(arr1, arr2)
+        assert np.allclose(arr1, arr3)
