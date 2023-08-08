623cf6de74abb72319b15e3fbf1a76afe84e919c
the-rccg <r.greif@outlook.com>
2023-08-08 18:03:35 +0200

    Add latex formatting and timestrace adjustments

---
+++ src/hw2d/utils/latex_format.py
@@ -0,0 +1,56 @@
+names = {
+    "int_gamma_n": "$\Gamma_n(t) = \int{dk_y \; \Gamma_n \left( k_y \\right) }$",
+    "integrated gamma_n": "$\Gamma_n(t) = \int{dk_y \; \Gamma_n \left( k_y \\right) }$",
+    "gamma_n": "$\Gamma_n$",
+    "gamma_n_k": "$\Gamma_n \left(k_y\\right)$",
+    "gamma_c": "$\Gamma_c$",
+    "ky_phi": "$\phi \left( k_y \\right)$",
+    "ky_omega": "$\Omega \left( k_y \\right)$",
+    "ky_density": "$n \left( k_y \\right)$",
+    "ky": "$k_y$",
+    "mean": "$\mu$",
+    "std": "$\sigma$",
+    "delta_k": "$\delta_k$",
+    "U_phi": "$U_\phi$",
+    "U_centered": "$U \left( \Omega-\mu \left( \Omega \\right), n \\right)$",
+    "nu": "$\\nu$",
+    "density": "density ($n$)",
+    "phi": "potential ($\phi$)",
+    "omega": "vorticity ($\Omega$)",
+    "lowerscale=16": "512x512 Downsampled (32x32)",
+    "raw": "Numerical Integrator (512x512)",
+    "model": "Our Model (32x32)",
+    "lowres": "Numerical Integrator (32x32)",
+    "dE_dt": "$\partial_t E$",
+    "dU_dt": "$\partial_t U$",
+    "DE_v": "$DE_v$",
+    "DE_n": "$DE_n$",
+    "DEv": "$DE_v$",
+    "DEn": "$DE_n$",
+    "DE": "$DE$",
+    "DU": "$DU$",
+    "E": "E",
+    "U": "U",
+    "cfl": "cfl",
+    "mean_gamma_n": "$\mu \left( \Gamma_n \\right)$",
+    "std_gamma_n": "$\sigma \left( \Gamma_n \\right)$",
+    "mean_gamma_c": "$\mu \left( \Gamma_c \\right)$",
+    "std_gamma_c": "$\sigma \left( \Gamma_c \\right)$",
+    "mean_int_gamma_n": "$\mu_t \left( \int \Gamma_n \left(k_y\\right) \\right)$",
+    "std_gamma_n": "$\sigma \left( \int \Gamma_n \left(k_y\\right) \\right)$",
+    "c1": "$c_1$",
+}
+
+
+def metric_format(string):
+    if any([s in string for s in ["mean", "median", "std"]]):
+        return string[: string.find("_")] + " " + names[string[string.find("_") + 1 :]]
+    return
+
+
+def latex_format(string):
+    try:
+        return names[string]
+    except Exception as e:
+        print(string, e)
+        return string
