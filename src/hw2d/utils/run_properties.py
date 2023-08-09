35b477efcc2fa7b49f484a46e3182fba7edfa85f
the-rccg <r.greif@outlook.com>
2023-08-09 18:15:16 +0200

    add debug and forcing to run_properties

--- src/hw2d/utils/run_properties.py
+++ src/hw2d/utils/run_properties.py
@@ -9,7 +9,7 @@ from hw2d.utils.io import *
 
 
 def main(
-    file_path: str = "test.h5",
+    file_path: str = "/ptmp/rccg/hw2d/512x512_dt=0.025_nu=1.0e-10.h5",
     t0: int = 0,
     t1: int = 0,
     batch_size=100,
@@ -21,6 +21,8 @@ def main(
         "enstrophy",
         "enstrophy_phi",
     ],
+    force_recompute: bool = False,
+    is_debug: bool = False,
 ):
     with h5py.File(file_path, "r+") as h5_file:
         parameters = dict(h5_file.attrs)
@@ -29,7 +31,9 @@ def main(
         property_run_list = []
         for property_name in property_list:
             if property_name in h5_file.keys():
-                del h5_file[property_name]
+                print(f"Dataset exists:  {property_name}")
+                if force_recompute:
+                    del h5_file[property_name]
             if property_name not in h5_file.keys():
                 property_run_list.append(property_name)
                 h5_file.create_dataset(property_name, (steps,))
@@ -54,6 +58,29 @@ def main(
             h5_file["enstrophy"][i : i + batch_size] = enstrophy
             enstrophy_phi = get_enstrophy_phi(n=n, phi=p, dx=parameters["dx"])
             h5_file["enstrophy_phi"][i : i + batch_size] = enstrophy_phi
+            # Debug printing
+            if is_debug:
+                print(
+                    f"Timeframe ({i*parameters['dt']}-{(i + batch_size)*parameters['dt']})"
+                )
+                print(
+                    "  |  ".join(
+                        [
+                            f"  Gamma_n: {np.mean(gamma_n):.2e}",
+                            f"Gamma_n(k): {np.mean(gamma_n_spectral):.2e}",
+                        ]
+                    )
+                )
+                print(f"  Gamma_c: {np.mean(gamma_c):2e}")
+                print(f"  Energy: {np.mean(energy):2e}")
+                print(
+                    "  |  ".join(
+                        [
+                            f"  Enstrophy: {np.mean(enstrophy):.2e}",
+                            f"Enstrophy(Phi): {np.mean(enstrophy_phi):.2e}",
+                        ]
+                    )
+                )
 
     return
 
