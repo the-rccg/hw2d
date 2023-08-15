e26755f64b09394e61453fde89355b7cb45ebd1d
Robin Greif <rccgreif@gmail.com>
2023-08-15 17:59:26 +0200

    add module level documentation to arakawa

--- src/hw2d/arakawa/__init__.py
+++ src/hw2d/arakawa/__init__.py
@@ -1,3 +1,21 @@
+"""
+The `hw2d.arakawa` Module
+=========================
+
+This module provides implementations of the Arakawa scheme to compute the Poisson bracket. 
+The Arakawa scheme ensures accurate computation of the Poisson bracket, avoiding spurious oscillations which can arise from naive discretization.
+
+The module offers implementations based on different computational frameworks:
+
+- **NumPy**: Efficient array operations and integration with the scientific Python ecosystem.
+- **Numba**: Just-in-time compiled functions optimized for speed.
+
+To see specific functions and their documentation, navigate to the respective sub-modules:
+
+- `hw2d.arakawa.numpy_arakawa`: Arakawa scheme functions implemented using NumPy.
+- `hw2d.arakawa.numba_arakawa`: Arakawa scheme functions accelerated with Numba.
+"""
+
 # # arakawa_scheme/__init__.py
 
 # from . import numpy_arakawa
