f22749eda8751f75e733293f2f9a1c52ce2b42ff
the-rccg <the-rccg@users.noreply.github.com>
2023-12-11 18:54:18 +0000

    deploy: abd6d862a719f84b26a93cb39b75506c0761f9ae

--- src/hw2d/__main__.py
+++
@@ -1,7 +0,0 @@
-import fire
-
-from .run import run
-
-
-if __name__ == "__main__":
-    fire.Fire(run)
--- tests/test_run.py
+++
@@ -1,5 +0,0 @@
-from hw2d.run import run
-
-
-def test_run(tmp_path):
-    run(end_time=0.5, movie=False, output_path=tmp_path / "test.h5")
